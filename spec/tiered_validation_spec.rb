cdf9e3448565124e9cd776f3057715778b020fe1
Kevin Hyland <khy@me.com>
2009-12-16 16:34:21 -0500

    Repeat tests for ActiveRecord subclass

--- spec/tiered_validation_spec.rb
+++ spec/tiered_validation_spec.rb
@@ -51,189 +51,210 @@ class Account < ActiveRecord::Base
   end
 end
 
-describe 'Validation tier convenience methods' do
-  describe '.create_with_[tier]_validation' do
-    it 'should create an instance using the specified tier validation' do
-      Account.create_with_user_validation!(:expiration_date => Time.now).should be_a_kind_of(Account)
+class Savings < Account
+end
+
+shared_examples_for 'a model with tiered validation, or one of its subclass' do
+  describe 'Validation tier convenience methods' do
+    describe '.create_with_[tier]_validation' do
+      it 'should create an instance using the specified tier validation' do
+        account_class.create_with_user_validation!(:expiration_date => Time.now).should be_a_kind_of(Account)
+      end
+  
+      it 'should raise a RecordInvalidForTier error if tier validation fails' do
+        lambda{account_class.create_with_user_validation!}.should raise_error(ActiveRecord::RecordInvalidForTier)
+      end
+    end
+
+    describe '#save_with_[tier]_validation' do
+      it 'should return true if instance is saved with tier validation' do
+        account_class.new(:expiration_date => Time.now).save_with_user_validation.should be_true
+      end
+  
+      it 'should return false if tier validation fails' do
+        account_class.new.save_with_user_validation.should be_false
+      end
     end
   
-    it 'should raise a RecordInvalidForTier error if tier validation fails' do
-      lambda{Account.create_with_user_validation!}.should raise_error(ActiveRecord::RecordInvalidForTier)
+    describe '#save_with_[tier]_validation!' do
+      it 'should return true if instance is saved with tier validation' do
+        account_class.new(:expiration_date => Time.now).save_with_user_validation!.should be_true
+      end
+  
+      it 'should raise a RecordInvalidForTier error if tier validation fails' do
+        lambda{account_class.new.save_with_user_validation!}.should raise_error(ActiveRecord::RecordInvalidForTier)
+      end
+    end
+  
+    describe '#valid_for_[tier]?' do
+      it 'should return false if tier validation fails' do
+        account_class.new(:expiration_date => Time.now).valid_for_user?.should be_true
+      end
+  
+      it 'should return false if tier validation fails' do
+        account_class.new.valid_for_user?.should be_false
+      end
+    end
+  
+    describe '#invalid_for_[tier]?' do
+      it 'should return false if tier validation passes' do
+        account_class.new(:expiration_date => Time.now).invalid_for_user?.should be_false
+      end
+  
+      it 'should return true if tier validation fails' do
+        account_class.new.invalid_for_user?.should be_true
+      end
     end
   end
 
-  describe '#save_with_[tier]_validation' do
-    it 'should return true if instance is saved with tier validation' do
-      Account.new(:expiration_date => Time.now).save_with_user_validation.should be_true
-    end
-  
-    it 'should return false if tier validation fails' do
-      Account.new.save_with_user_validation.should be_false
+  describe 'Exclusive validation tier' do
+    it 'should not share validations with the standard validation chain' do
+      account = account_class.new
+      account.should_not be_valid_for_user
+      account.should be_valid
     end
   end
-  
-  describe '#save_with_[tier]_validation!' do
-    it 'should return true if instance is saved with tier validation' do
-      Account.new(:expiration_date => Time.now).save_with_user_validation!.should be_true
-    end
-  
-    it 'should raise a RecordInvalidForTier error if tier validation fails' do
-      lambda{Account.new.save_with_user_validation!}.should raise_error(ActiveRecord::RecordInvalidForTier)
+
+  describe 'Non-exclusive validation tier' do
+    it 'should share validations with the standard validation chain' do
+      account = account_class.new(:number => 'AAAAAA')
+      account.should_not be_valid_for_monkey
+      account.should_not be_valid
     end
   end
-  
-  describe '#valid_for_[tier]?' do
-    it 'should return false if tier validation fails' do
-      Account.new(:expiration_date => Time.now).valid_for_user?.should be_true
-    end
-  
-    it 'should return false if tier validation fails' do
-      Account.new.valid_for_user?.should be_false
+
+  describe 'Validation tier with included tier' do
+    it 'should use included tier validations' do
+      account = account_class.new(:preferred => true)
+      account.should_not be_valid_for_cowboy
     end
   end
-  
-  describe '#invalid_for_[tier]?' do
-    it 'should return false if tier validation passes' do
-      Account.new(:expiration_date => Time.now).invalid_for_user?.should be_false
+
+  describe 'Validation tier' do
+    it 'should use on create option' do
+      account = account_class.new(:number => '1238')
+      account.should_not be_valid_for_pirate
+      account.number = '1235'
+      account.should_not be_valid_for_pirate
+      account.number = '1234'
+      account.should be_valid_for_pirate
+    end
+
+    it 'should use on update option' do
+      account = account_class.new(:number => '1234')
+      account.save
+      account.should_not be_valid_for_pirate
     end
   
-    it 'should return true if tier validation fails' do
-      Account.new.invalid_for_user?.should be_true
+    it 'should include any default validations' do
+      account = account_class.new(:number => '1ab', :expiration_date => 2.days.from_now)
+      account.should_not be_valid_for_user
+    end
+  end
+
+  describe 'Validation tier' do
+    def build_account
+      account = account_class.new(:balance => 50, :eula => true, :number => '123456',
+        :number_confirmation => '123456')
+      account.login.username = 'jah123'
+      account
+    end
+
+    before(:all) do
+      account_class.delete_all
+    end
+
+    before(:each) do
+      @account = build_account
+      @account.should be_valid_for_admin
+    end
+
+    it 'should appropriately apply validates_acceptance_of' do
+      @account.eula = false
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:eula).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_associated' do
+      @account.login.username = nil
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:login).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_confirmation_of' do
+      @account.number_confirmation = '135791'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_each' do
+      @account.number = '111111'
+      @account.number_confirmation = '111111'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_exclusion_of' do
+      @account.number = '123581'
+      @account.number_confirmation = '123581'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_format_of' do
+      @account.number = '234567'
+      @account.number_confirmation = '234567'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_inclusion_of' do
+      @account.balance = -5
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:balance).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_length_of' do
+      @account.number = '1234567'
+      @account.number_confirmation = '1234567'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_numericality_of' do
+      @account.number = '123ABC'
+      @account.number_confirmation = '123ABC'
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:number).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_presence_of' do
+      @account.balance = nil
+      @account.should_not be_valid_for_admin
+      @account.errors.on(:balance).should_not be_nil
+    end
+
+    it 'should appropriately apply validates_presence_of' do
+      @account.save
+      new_account = build_account
+      new_account.should_not be_valid_for_admin
+      new_account.errors.on(:number).should_not be_nil
     end
   end
 end
 
-describe 'Exclusive validation tier' do
-  it 'should not share validations with the standard validation chain' do
-    account = Account.new
-    account.should_not be_valid_for_user
-    account.should be_valid
+describe Account do
+  def account_class
+    Account
   end
+
+  it_should_behave_like 'a model with tiered validation, or one of its subclass'
 end
 
-describe 'Non-exclusive validation tier' do
-  it 'should share validations with the standard validation chain' do
-    account = Account.new(:number => 'AAAAAA')
-    account.should_not be_valid_for_monkey
-    account.should_not be_valid
-  end
-end
-
-describe 'Validation tier with included tier' do
-  it 'should use included tier validations' do
-    account = Account.new(:preferred => true)
-    account.should_not be_valid_for_cowboy
-  end
-end
-
-describe 'Validation tier' do
-  it 'should use on create option' do
-    account = Account.new(:number => '1238')
-    account.should_not be_valid_for_pirate
-    account.number = '1235'
-    account.should_not be_valid_for_pirate
-    account.number = '1234'
-    account.should be_valid_for_pirate
+describe Savings do
+  def account_class
+    Savings
   end
 
-  it 'should use on update option' do
-    account = Account.new(:number => '1234')
-    account.save
-    account.should_not be_valid_for_pirate
-  end
-  
-  it 'should include any default validations' do
-    account = Account.new(:number => '1ab', :expiration_date => 2.days.from_now)
-    account.should_not be_valid_for_user
-  end
-end
-
-describe 'Validation tier' do
-  def build_account
-    account = Account.new(:balance => 50, :eula => true, :number => '123456',
-      :number_confirmation => '123456')
-    account.login.username = 'jah123'
-    account
-  end
-
-  before(:all) do
-    Account.delete_all
-  end
-
-  before(:each) do
-    @account = build_account
-    @account.should be_valid_for_admin
-  end
-
-  it 'should appropriately apply validates_acceptance_of' do
-    @account.eula = false
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:eula).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_associated' do
-    @account.login.username = nil
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:login).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_confirmation_of' do
-    @account.number_confirmation = '135791'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_each' do
-    @account.number = '111111'
-    @account.number_confirmation = '111111'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_exclusion_of' do
-    @account.number = '123581'
-    @account.number_confirmation = '123581'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_format_of' do
-    @account.number = '234567'
-    @account.number_confirmation = '234567'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_inclusion_of' do
-    @account.balance = -5
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:balance).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_length_of' do
-    @account.number = '1234567'
-    @account.number_confirmation = '1234567'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_numericality_of' do
-    @account.number = '123ABC'
-    @account.number_confirmation = '123ABC'
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:number).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_presence_of' do
-    @account.balance = nil
-    @account.should_not be_valid_for_admin
-    @account.errors.on(:balance).should_not be_nil
-  end
-
-  it 'should appropriately apply validates_presence_of' do
-    @account.save
-    new_account = build_account
-    new_account.should_not be_valid_for_admin
-    new_account.errors.on(:number).should_not be_nil
-  end
+  it_should_behave_like 'a model with tiered validation, or one of its subclass'
 end
